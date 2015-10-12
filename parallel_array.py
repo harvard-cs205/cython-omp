2a7d3a4b9892be3a54c0325f0fcb108ebf831444
Thouis (Ray) Jones <thouis@gmail.com>
2015-10-12 15:24:34 -0400

    remove old python code

--- parallel_array.py
+++
@@ -1,9 +0,0 @@
-import set_compiler
-set_compiler.install()
-import pyximport
-pyximport.install()
-
-import omp
-
-omp.parallel_array()
-
--- parallel_block_with_lock.py
+++
@@ -1,8 +0,0 @@
-import set_compiler
-set_compiler.install()
-import pyximport
-pyximport.install()
-
-import omp
-
-omp.parallel_block_with_lock()
